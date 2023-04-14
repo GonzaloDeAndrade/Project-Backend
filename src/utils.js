e915cf1d7f72252adbc9c2c36c463baa01a882a3
Gonzalo de Andrade <gonzadeandrade1@gmail.com>
2023-04-13 23:50:35 -0300

    login y register

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,8 @@
+import path from "path";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
 
+// Debemos crear nuestra propia variable __dirname a través de este método si usamos ESM
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 export default __dirname;
