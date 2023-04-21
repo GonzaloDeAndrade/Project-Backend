9bc6508de50e60aa7bc95bb90bd7fb76a31d4d53
Gonzalo de Andrade <gonzadeandrade1@gmail.com>
2023-04-20 23:42:39 -0300

    estable con crypt y passport

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,60 +1,92 @@
 import { Router } from "express";
 import { UserModel } from "../dao/models/user.model.js";
+import { createHash, isValidPassword, generateToken, validateToken} from "../server/utils.js";
+import passport from "passport";
 
 const AuthRouter = Router();
 
-
 //register
-AuthRouter.post("/signup", async(req, res)=>{
-    try {
-        const {email, password} = req.body;
-        const user = await UserModel.findOne({email:email});
-        if (!user) {
-            const newUser = await UserModel.create({email, password});
-            req.session.user = newUser.email;
-            req.session.rol = "user";
-            if (email = "adminCoder@coder.com") {
-                req.session.rol = "admin";
-                console.log(req.session);
-            }
-            //ejemplo a profile en este caso va a products
-            return res.redirect("/products");
-        } else {
-            res.send(`Usuario ya registrado <a href="/login">Iniciar sesion </a>`)
-        }
-    } catch (error) {
-        console.log(error);
-    }
+AuthRouter.post(
+  "/signup",
+  passport.authenticate("signupStrategy", {
+    failureRedirect: "/api/sessions/failure-signup",
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    req.session.username = req.session.user.email;
+    req.session.rol = "user"
+    res.send("usuario registrado");
+  }
+);
+//falla de registro
+AuthRouter.get("/failure-signup", (req, res) => {
+  res.send("falla en el registro");
 });
+//reg por github
+AuthRouter.get("/github", passport.authenticate("githubSignup",{scope:["user:email"]}),
+async (req,res) =>{
+
+});
+//callback de github
+AuthRouter.get(
+  "/github-callback",
+  passport.authenticate("githubSignup", {
+    failureRedirect: "/api/sessions/failure-signup",
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    req.session.username = req.session.user.email;
+    req.session.rol = "user"
+    console.log(req.session.user);  
+    res.send("usuario autenticado");
+  }
+);
+
 //login
-AuthRouter.post("/login", async (req,res) => {
-    const {email, password} = req.body;
-    const authorized = await UserModel.findOne({email:email,password:password});
-    if(!authorized){
-        res.send("ususario no identificado");
-    }else{
-        if (email === "adminCoder@coder.com") {
-            req.session.user = email;
+AuthRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const authorized = await UserModel.findOne({ email: email }).lean();
+  if (!authorized) {
+    res.send("ususario no identificado");
+  } else {
+    if (email === "adminCoder@coder.com") {
+        if (isValidPassword(authorized, password)) {
+            req.session.user = authorized._id;
+            req.session.username = email;
             req.session.rol = "admin";
-            console.log(req.session);
-        }else{
-            req.session.user = email;
+            console.log(req.session.rol);
+            return res.send("login exitoso");
+          } else {
+            res.send("credenciales invalidas");
+          }
+    } else {
+      if (isValidPassword(authorized, password)) {
+        req.session.user = authorized._id;
+        req.session.username = email;
         req.session.rol = "user";
-        }
-        return res.redirect("/products");
-    }  
-    
-})
+        return res.send("login exitoso");
+      } else {
+        res.send("credenciales invalidas");
+      }
+    }
+    return res.redirect("/products");
+  }
+});
 
 //logOut
-AuthRouter.post("/logout", (req,res) =>{
-    req.session.destroy(error => {
+AuthRouter.post("/logout", (req, res) => {
+  req.logOut((error) => {
+    if (error) {
+      return res.send("no se pudo cerrar la sesion");
+    } else {
+      req.session.destroy((err) => {
         if (error) {
-            return res.send ("no se pudo cerrar la sesion");
-        } else {
-            return res.redirect("/login");
+          return res.send("no se pudo cerrar la sesion");
         }
-    });
+        res.redirect("/login");
+      });
+    }
+  });
 });
 
-export {AuthRouter}
+export { AuthRouter };
